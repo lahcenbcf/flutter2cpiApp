2dfb595c1ff432a236e8a5e376a6636e478f8fea
lahcenbcf <h.bencharef@esi-sba.dz>
2023-03-07 10:10:54 +0100

    add updates

--- lib/constants/theme_Styles.dart
+++ lib/constants/theme_Styles.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 class ThemeInfo{
- static const Color primaryColor=Color(0xff292d36);
+ static const primaryColor=Color(0xff292d36);
   static const Color secondaryColor=Color(0xff20C57A);
   static final fontFamily=GoogleFonts.poppins().fontFamily;
 }
